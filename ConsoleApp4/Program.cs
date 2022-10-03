421f0969213a7f98c894bb7aac4b3af02bdf05a8
akashkuchankar <kuchankarakash@gmail.com>
2022-10-03 08:48:35 -0700

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any number");
+                int a= Convert.ToInt32(Console.ReadLine());
+            if(a==0)
+            {
+                Console.WriteLine("number is zero");
+              }
+            if(a>0)
+                {
+                Console.WriteLine("number is positive");
+            }else 
+            {
+                Console.WriteLine("number is negative");
+            }
+            Console.ReadLine();  
+        }
+    }
+}
